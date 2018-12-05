86ab9a7ca5783dec281c739dff7a331e77b32916
Scott Erwin <StephaniesHusband@gmail.com>
2018-12-05 11:41:28 -0600

    multiple updates for expanding functionality for other sprint. added feature flags to surround it

--- demo.js
+++ demo.js
@@ -1,5 +1,8 @@
 (function() {
    var vm;
+   var FeatureFlags = {
+      OtherSprint: false
+   };
 
    Vue.component("story-card", {
       template: "#story-card",
@@ -21,14 +24,37 @@
       }
    });
 
-   $.getJSON("./stories.json", function(storiesData) {
+   $.getJSON("./stories.json", function(sprintData) {
       vm = new Vue({
          el: "#app",
-         data: storiesData
+         data: $.extend({}, sprintData, {
+            FeatureFlags: FeatureFlags
+         })
       });
 
-      $(".img-yoda").click(function() {
-         $(".complete").fadeToggle("slow");
+      $("footer").click(function() {
+         $(".img-yoda").fadeToggle("slow", function() {
+            $(".complete").fadeToggle("slow");
+         });
       });
+
+      if (FeatureFlags.OtherSprint) {
+         $(".other-sprint").click(function() {
+            var $this = $(this);
+
+            if ($this.data("sprint") === 1) {
+               $this.data("sprint", 2);
+               $this.html("This Sprint");
+
+               $.getJSON("./other_sprint_stories.json", function(sprintData) {
+                  vm.release = sprintData.release;
+                  vm.stories = sprintData.stories;
+               });
+            }
+            else {
+               document.location.reload();
+            }
+         });
+      }
    });
 })();
